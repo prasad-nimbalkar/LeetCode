289110e7d84337c63fd125a2f0ffaf5495179b7a
Prasad Nimbalkar <54078414+prasad-nimbalkar@users.noreply.github.com>
2025-10-07 08:53:47 +0530

    Time: 2 ms (64.23%), Space: 17.9 MB (22.04%) - LeetHub

--- 3636-check-balanced-string/3636-check-balanced-string.py
+++ 3636-check-balanced-string/3636-check-balanced-string.py
@@ -2,9 +2,16 @@ class Solution:
     def isBalanced(self, num: str) -> bool:
         e_sum, o_sum = 0, 0
 
-        for i, digit in enumerate(num):
+        for i in range(len(num)):
             if i % 2 == 0:
-                e_sum += int(digit)
+                e_sum += int(num[i])
             else:
-                o_sum += int(digit)
+                o_sum += int(num[i])
         return e_sum == o_sum
+
+        # for i, digit in enumerate(num):
+        #     if i % 2 == 0:
+        #         e_sum += int(digit)
+        #     else:
+        #         o_sum += int(digit)
+        # return e_sum == o_sum
