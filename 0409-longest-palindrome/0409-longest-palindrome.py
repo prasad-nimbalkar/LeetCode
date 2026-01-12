1b39ef8bcff17b915a3c36c804bf662bcd98de0f
Prasad Nimbalkar <54078414+prasad-nimbalkar@users.noreply.github.com>
2026-01-12 21:57:49 +0530

    Time: 3 ms (44.69%), Space: 19.3 MB (5.59%) - LeetxGit

---
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        seen = set()
+        res =0
+
+        for c in s:
+            if c in seen:
+                seen.remove(c)
+                res += 2
+            else:
+                seen.add(c)
+        
+        if seen:
+            res +=1
+            
+        return res
+
+# Time: O(n)
+# Space: O(n)
